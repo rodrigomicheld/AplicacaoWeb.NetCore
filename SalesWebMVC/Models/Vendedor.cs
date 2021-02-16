2a1723f0406d7bd90835191732e7c4a8f77a205b
Rodrigo Michel <rodrigo.micheld@gmail.com>
2021-02-16 17:56:02 -0300

    Validações do campos com javaScript e com Framework

--- SalesWebMVC/Models/Vendedor.cs
+++ SalesWebMVC/Models/Vendedor.cs
@@ -8,16 +8,26 @@ namespace SalesWebMVC.Models
     public class Vendedor
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [StringLength(30,MinimumLength = 3,ErrorMessage = "O {0} não atentede o tamanho minimo de {2} e máximo {1} caracterer(s) ")]
         [Display(Name = "Nome do Vendedor")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Informe um email válido")]
         public string Email { get; set; }
+        
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         public DateTime DataNasciemnto { get; set; }
+        
         [Display(Name = "Salário")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         public double Salario { get; set; }
         public Departamento Departamento { get; set; }
         [Display(Name = "Departamento")]
